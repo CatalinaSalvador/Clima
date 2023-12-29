856e6a44e51fc809a8df7228c7593e237875b757
MonserratRivas <catalinarivas.iccp@gmail.com>
2023-12-29 04:25:06 -0300

    iniciando repostorio

---
+++ script.js
@@ -0,0 +1,27 @@
+/*eliminar alerta */
+function eliminarAlert(){
+    const Alert = document.querySelector(".cuadro-alerta")
+    Alert.parentNode.removeChild(Alert)
+}
+
+/*°c y °f*/
+function c2f(temp) {
+    return Math.round(9 / 5 * temp + 32);
+}
+
+function f2c(temp) {
+    return Math.round(5 / 9 * (temp - 32));
+}
+
+function convert(element) {
+    console.log(element.value);
+    for(var i=1; i<9; i++) {
+        var tempSpan = document.querySelector("#temp" + i);
+        var tempVal = parseInt(tempSpan.innerText);
+        if(element.value == "°C") {
+            tempSpan.innerText = f2c(tempVal);
+        } else {
+            tempSpan.innerText = c2f(tempVal);
+        }
+    }
+}
